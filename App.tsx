79396ec3489034c9cf7fd32deedad53a9d2ace25
Erikson Gonçalves Bezerra <erikson.g.bezerra@bradesco.com.br>
2024-09-04 18:03:50 -0300

    feat: config push notification android

--- App.tsx
+++ App.tsx
@@ -4,10 +4,15 @@ import { StatusBar } from "expo-status-bar";
 import "intl";
 import { useCallback } from "react";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { OneSignal } from "react-native-onesignal";
 import { ThemeProvider } from "styled-components";
+import { env } from "./src/config/env";
 
 import { Routes } from "@routes/index";
 import { theme } from "@theme/index";
+
+OneSignal.initialize(env.EXPO_PUBLIC_ONE_SIGNAL_API_KEY);
+OneSignal.Notifications.requestPermission(true);
 
 export default function App() {
   const [fontsLoaded] = useFonts({
