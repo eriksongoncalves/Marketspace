2b393ecdc45d444e59c40a172b12b718eec4b6d2
Erikson Gonçalves Bezerra <erikson.g.bezerra@bradesco.com.br>
2024-09-08 15:53:22 -0300

    feat: deep linking

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,33 @@
-import { NavigationContainer } from "@react-navigation/native";
+import { LinkingOptions, NavigationContainer } from "@react-navigation/native";
 import React from "react";
 
 import { AuthStackRoutes } from "./auth.stack.routes";
 
+const linking: LinkingOptions<ReactNavigation.RootParamList> = {
+  prefixes: ["marketspace://", "com.teste.marketspace://"],
+  config: {
+    screens: {
+      app: {
+        path: "/app",
+        exact: true,
+        screens: {
+          initialRouteName: "home_tab",
+          detail: {
+            path: "/app/detail/:id",
+            parse: {
+              id: (id: string) => id
+            },
+            exact: false
+          }
+        }
+      }
+    }
+  }
+};
+
 export function Routes() {
   return (
-    <NavigationContainer>
+    <NavigationContainer linking={linking}>
       <AuthStackRoutes />
     </NavigationContainer>
   );
